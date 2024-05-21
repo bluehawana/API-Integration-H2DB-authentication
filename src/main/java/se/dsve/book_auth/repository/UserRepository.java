2919d8c470c7d1fbe5f4afe59c0ad5b59001d8f3
Lars Strömberg <lars@dsve.se>
2024-05-21 16:06:22 +0000

    Add UserRepository.java

---
+++ src/main/java/se/dsve/book_auth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package se.dsve.book_auth.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import se.dsve.book_auth.model.User;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
