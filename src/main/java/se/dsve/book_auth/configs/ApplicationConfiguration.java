25dae8c84a9dfb647db12ae6f0804b105edef64e
Lars Strömberg <lars@dsve.se>
2024-05-21 16:06:24 +0000

    Add ApplicationConfiguration.java

---
+++ src/main/java/se/dsve/book_auth/configs/ApplicationConfiguration.java
@@ -0,0 +1,48 @@
+package se.dsve.book_auth.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import se.dsve.book_auth.repository.UserRepository;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    private final UserRepository userRepository;
+
+    public ApplicationConfiguration(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        // TODO: Wriite your code here
+        return null;
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        // TODO: Wriite your code here
+        return null;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        // TODO: Wriite your code here
+        return null;
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+}
