101528f0d190fdefd2ffef5c269ca08ce9d0f678
Lars Strömberg <lars@dsve.se>
2024-05-21 16:06:26 +0000

    Add LoginResponse.java

---
+++ src/main/java/se/dsve/book_auth/model/LoginResponse.java
@@ -0,0 +1,13 @@
+package se.dsve.book_auth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private long expiresIn;
+}
