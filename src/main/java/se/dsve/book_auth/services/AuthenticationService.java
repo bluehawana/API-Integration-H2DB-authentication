731ee3646334bfbd4b8de066c09a72f7b784c784
Hongzhi Li <bluehawana@gmail.com>
2024-05-28 18:40:44 +0200

    modify authenticationservice.java and others

--- src/main/java/se/dsve/book_auth/services/AuthenticationService.java
+++ src/main/java/se/dsve/book_auth/services/AuthenticationService.java
@@ -1,17 +1,13 @@
 package se.dsve.book_auth.services;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import se.dsve.book_auth.dtos.LoginUserDto;
 import se.dsve.book_auth.dtos.RegisterUserDto;
 import se.dsve.book_auth.model.User;
 import se.dsve.book_auth.repository.UserRepository;
-
 
 @Service
 public class AuthenticationService {
@@ -19,34 +15,33 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
-
     public AuthenticationService(
             UserRepository userRepository,
             AuthenticationManager authenticationManager,
-            PasswordEncoder passwordEncoder) {
+            PasswordEncoder passwordEncoder
+    ) {
         this.userRepository = userRepository;
         this.authenticationManager = authenticationManager;
         this.passwordEncoder = passwordEncoder;
     }
 
     public User signup(RegisterUserDto input) {
-        // TODO: Write your code here
         User user = User.builder()
                 .email(input.getEmail())
                 .password(passwordEncoder.encode(input.getPassword()))
+                .fullName(input.getFullName())
                 .build();
         return userRepository.save(user);
     }
 
     public User authenticate(LoginUserDto input) {
-        // TODO: Write your code here
-        User user = (User) userRepository.findByEmail(input.getEmail())
-                .orElseThrow(() -> new BadCredentialsException("Invalid email or password"));
-
-        if (!passwordEncoder.matches(input.getPassword(), user.getPassword())) {
-            throw new BadCredentialsException("Invalid email or password");
-        }
-
-        return user;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+        return (User) userRepository.findByEmail(input.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
